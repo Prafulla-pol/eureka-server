b270fc02367cba00889b54cc1ac09ed892b16e5c
Prafulla Pol <prafulla.pol@sailpoint.com>
2021-05-16 22:11:29 +0530

    Eureka client service

---
+++ eureka-client-service/src/test/java/com/springcloud/eurekaclientservice/EurekaClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.eurekaclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
